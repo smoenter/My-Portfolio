648d364f6fcf3e2dfea07dc0a0a512140c3736eb
smoenter <staceymoenter@gmail.com>
2025-02-11 19:37:49 -0500

    initial coding for Contact

--- My Portfolio/src/views/Contact.jsx
+++ My Portfolio/src/views/Contact.jsx
@@ -1,7 +1,98 @@
-import React from 'react'
+import {useState} from 'react';
 
-export default function Contact() {
+// Here we import a helper function that will check if the email is valid
+import { checkMessage, validateEmail } from './utils/helpers';
+
+
+function Contact () {
+  // setting state variables for name, email and message using `useState`
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, name, and message
+    
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'name') {
+      setName(inputValue);
+    } else if (inputType === 'message'){
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+  // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+  if (!validateEmail(email) || !name) {
+    setErrorMessage('Email or Name is invalid');
+    // We want to exit out of this code block if something is wrong so that the user can correct it
+    return;
+    // Then we check to see if the user input a message. If no message then we set an error message regarding the no message.
+  }
+  if (!checkMessage(message)) {
+    setErrorMessage(
+      `Message is required: `
+    );
+    return;
+  }
+ // If successful, we want to clear out the input after registration.
+ setName('');
+ setMessage('');
+ setEmail('');
+ alert(`Hello ${name}`);
+};
+
   return (
-    <div>Contact</div>
-  )
+    <div className="container text-center">
+      <h1>
+       Contact
+      </h1>
+      <form className="form" onSubmit={handleFormSubmit}>
+        <input
+          value={name}
+          name="name"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Email"
+        />
+          <input
+          value={message}St
+          name="message"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Message"
+        />
+        <button type="submit">
+          Submit
+        </button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
+    </div>
+  );
 }
+
+
+export default Contact;
+  
+
---
+++ My Portfolio/src/views/utils/helpers.js
@@ -0,0 +1,12 @@
+export function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+export function checkMessage(input) {
+  const message = /^[A-Za-z]\w{7,14}$/;
+  if (input.match(message)) {
+    return true;
+  }
+  return false;
+}
