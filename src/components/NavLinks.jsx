62c7f1be2efc24c695bbef355386bfdc6741cddb
smoenter <staceymoenter@gmail.com>
2025-02-16 09:58:43 -0500

    updated styling to contact

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Contact from '../views/Contact';
 
-export default function NavLinks() {
+function ColorSchemesExample() {
   return (
-    <div>NavLinks</div>
-  )
+    <>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#AboutMe">About Me</Nav.Link>
+            <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="#Contact">Contact</Nav.Link>
+            <Nav.Link href="#Resume">Resume</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
+
+export default ColorSchemesExample;
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,11 +1,12 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
 
 // Here we import a helper function that will check if the email is valid
 import { checkMessage, validateEmail } from './utils/helpers';
 
 
-function Contact () {
-  // setting state variables for name, email and message using `useState`
+function Contact() {
+  // setting state variables for name, email and message, and error message using `useState`
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
@@ -18,12 +19,12 @@ function Contact () {
     const inputValue = target.value;
 
     // Based on the input type, we set the state of either email, name, and message
-    
+
     if (inputType === 'email') {
       setEmail(inputValue);
     } else if (inputType === 'name') {
       setName(inputValue);
-    } else if (inputType === 'message'){
+    } else if (inputType === 'message') {
       setMessage(inputValue);
     }
   };
@@ -32,53 +33,53 @@ function Contact () {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
 
-  // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-  if (!validateEmail(email) || !name) {
-    setErrorMessage('Email or Name is invalid');
-    // We want to exit out of this code block if something is wrong so that the user can correct it
-    return;
-    // Then we check to see if the user input a message. If no message then we set an error message regarding the no message.
-  }
-  if (!checkMessage(message)) {
-    setErrorMessage(
-      `Message is required: `
-    );
-    return;
-  }
- // If successful, we want to clear out the input after registration.
- setName('');
- setMessage('');
- setEmail('');
- alert(`Hello ${name}`);
-};
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email) || !name) {
+      setErrorMessage('Email or Name is invalid');
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the user input a message. If no message then we set an error message regarding the no message.
+    }
+    if (!checkMessage(message)) {
+      setErrorMessage(
+        `Message is required: `
+      );
+      return;
+    }
+    // If successful, we want to clear out the input after registration.
+    setName('');
+    setMessage('');
+    setEmail('');
+    alert(`Hello ${name}`);
+  };
 
   return (
     <div className="container text-center">
       <h1>
-       Contact
+        Contact
       </h1>
       <form className="form" onSubmit={handleFormSubmit}>
-        <input
-          value={name}
-          name="name"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Name"
-        />
-        <input
-          value={email}
-          name="email"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Email"
-        />
-          <input
-          value={message}St
+      <Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="name" placeholder="name" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Message</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+        </Form>
+        {/* <input
+          value={message}
           name="message"
           onChange={handleInputChange}
           type="text"
           placeholder="Message"
-        />
+        /> */}
         <button type="submit">
           Submit
         </button>
@@ -94,5 +95,5 @@ function Contact () {
 
 
 export default Contact;
-  
+
 
