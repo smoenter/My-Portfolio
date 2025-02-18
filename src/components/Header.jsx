f78e6eeab5aa6f48a1baf811848a943610466448
smoenter <staceymoenter@gmail.com>
2025-02-18 18:51:20 -0500

    moved the navlinks to the right of the page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
  
 
 function Header() {
@@ -7,10 +8,10 @@ function Header() {
     <header className="header">
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">Stacey Moenter | Full Stack Developer</Navbar.Brand>
+          <Navbar.Brand as= {Link} to="/">Stacey Moenter | Full Stack Developer</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto" >
+            <Nav className="ms-auto" >
               <Nav.Link href="/about">About Me</Nav.Link>
               <Nav.Link href="/portfolio">Portfolio</Nav.Link>
               <Nav.Link href="/contact">Contact</Nav.Link>
